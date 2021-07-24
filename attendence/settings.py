00b12a9455a2216cc80526b6718b061b07c98928
Warningsvirus27 <warningsvirus27@gmail.com>
2021-07-24 11:14:41 +0530

    First commit

--- attendence/settings.py
+++ attendence/settings.py
@@ -43,10 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
-    # requirement for hosting on heroku
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,7 +129,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/'
 
--- attendence/urls.py
+++ attendence/urls.py
@@ -1,19 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# needed for hosting on heroku
-import settings
-from django.views.static import serve
-from django.conf.urls import url
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('page.urls')),
     path('', include('django.contrib.auth.urls')),
 
-    # needed for hosting on heroku
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
